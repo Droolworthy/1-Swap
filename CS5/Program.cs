321e8da2f6fb3dc458f1659954b3c95e72dfdffc
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-13 16:42:28 +0300

    Добавьте файлы проекта.

---
+++ CS5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CS5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vladimir";
+            string family = "Hohlov";
+            (name, family) = (family, name);
+            Console.WriteLine($"{family}, {name}.");
+            Console.WriteLine($"{name}, {family}.");
+        }
+    }
+}
